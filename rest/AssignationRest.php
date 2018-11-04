6a41fe6e0888e152532ede8c9002a873e625857f
MrKapplan <ivanddf1994@gmail.com>
2018-11-04 13:42:07 +0100

    Update rest/AssignationRest.php

--- rest/AssignationRest.php
+++ rest/AssignationRest.php
@@ -30,18 +30,18 @@ class AssignationRest extends BaseRest {
     public function add($pollLink, $data) {
 		$currentUser = parent::authenticateUser();
         $poll = $this->pollMapper->findPollByLink($pollLink);
-        $gaps = $this->gapMapper->findGapsByIdPoll($poll->getId());
+        //$gaps = $this->gapMapper->findGapsByIdPoll($poll->getId());
 
         if($poll == NULL){
             header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
 			header('Content-Type: application/json');
-        }else if($gaps == NULL){
-            header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
-            header('Content-Type: application/json');
+        // }else if($gaps == NULL){
+        //     header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
+        //     header('Content-Type: application/json');
         }else {
             
 			try {
-                
+
 				// validate Gaps object
 				//$this->gapMapper->checkForAdd_Updates($data);
 
@@ -67,48 +67,50 @@ class AssignationRest extends BaseRest {
 	}
 
 
-	// public function edit($pollLink, $data) {
-	// 	$currentUser = parent::authenticateUser();
-    //     $poll = $this->pollMapper->findPollByLink($pollLink);
+	    public function edit($pollLink, $data) {
+		$currentUser = parent::authenticateUser();
+        $poll = $this->pollMapper->findPollByLink($pollLink);
+        //$gaps = $this->gapMapper->findGapsByIdPoll($poll->getId());
 
-    //     if($poll == NULL){
-    //         header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
-	// 		header('Content-Type: application/json');
-    //     } else if($currentUser->getUsername() != $poll->getAuthor()->getUsername()){
-    //         header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
-	// 		echo("You are not authorized to add gap if you are not the poll's author");
-    //     } else{
-	// 		try {
-	// 			$gaps = $this->gapMapper->findGapsByIdPoll($poll->getId());
+        if($poll == NULL){
+            header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
+			header('Content-Type: application/json');
+        // }else if($gaps == NULL){
+        //     header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
+        //     header('Content-Type: application/json');
+        // 
+        }else {
+            
+			try {
+                
+				// validate Gaps object
+				//$this->gapMapper->checkForAdd_Updates($data);
 
-	// 			// validate Gaps object
-	// 			$this->gapMapper->checkForAdd_Updates($data);
+				// save the Assignation object into the database
+				$this->assignationMapper->update($currentUser->getUsername(), $data, $poll->getId());
 
-	// 			// save the Gaps object into the database
-	// 			$this->gapMapper->updateGaps($data, $poll->getId(), $gaps);
+				// response OK. Also send post in content
+				header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+				//header('Location: /meetPoll_TSW/rest/poll/'.$pollLink);
+				//header('Content-Type: application/json');
+				// echo(json_encode(array(
+				// 	"date"=>$postId,
+				// 	"start"=>$post->getTitle(),
+				// 	"end" => $post->getContent()
+				// )));
 
-	// 			// response OK. Also send post in content
-	// 			header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-	// 			//header('Location: /meetPoll_TSW/rest/poll/'.$pollLink);
-	// 			//header('Content-Type: application/json');
-	// 			// echo(json_encode(array(
-	// 			// 	"date"=>$postId,
-	// 			// 	"start"=>$post->getTitle(),
-	// 			// 	"end" => $post->getContent()
-	// 			// )));
-
-	// 		} catch (ValidationException $e) {
-	// 			header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
-	// 			header('Content-Type: application/json');
-	// 			echo(json_encode($e->getErrors()));
-	// 		}
-	// 	}
-	// }
+			} catch (ValidationException $e) {
+				header($_SERVER['SERVER_PROTOCOL'].' 400 Bad request');
+				header('Content-Type: application/json');
+				echo(json_encode($e->getErrors()));
+			}
+		}
+	}
 
 }
 
 // URI-MAPPING for this Rest endpoint
 $assignationRest = new AssignationRest();
 URIDispatcher::getInstance()
-->map("POST", "/assignation/$1", array($assignationRest, "add"));
-// ->map("PUT", "/gap/$1", array($gapRest, "edit"));
+->map("POST", "/assignation/$1", array($assignationRest, "add"))
+->map("PUT", "/assignation/$1", array($assignationRest, "edit"));
